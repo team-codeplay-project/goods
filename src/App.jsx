cb1d707bccf86ac7145da6f6e27b0cff32813a23
yonghyuk <sungyh5252@gmail.com>
2023-07-08 23:10:10 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      https://ojsfile.ohmynews.com/STD_IMG_FILE/2022/0901/IE003044103_STD.jpg
+      https://lgcxydabfbch3774324.cdn.ntruss.com/KBO_FILE/notice%2fimages%2f2018%2f6%2f%ed%99%88%ed%8e%98%ec%9d%b4%ec%a7%80%ec%9a%a93939.jpg
     </div>
   );
 }
