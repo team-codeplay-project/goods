bb4ecb6a2978306aa8d9faddb793342ef134cad2
yonghyuk <sungyh5252@gmail.com>
2023-07-08 23:11:25 +0900

    굿즈 url

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div>
-      https://ojsfile.ohmynews.com/STD_IMG_FILE/2022/0901/IE003044103_STD.jpg
-      https://lgcxydabfbch3774324.cdn.ntruss.com/KBO_FILE/notice%2fimages%2f2018%2f6%2f%ed%99%88%ed%8e%98%ec%9d%b4%ec%a7%80%ec%9a%a93939.jpg
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
